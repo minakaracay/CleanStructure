b2c8a861b2b0a704f3ef4d1a36d81a2bed672db9
Mina Karaçay <minakaracayy@gmail.com>
2026-05-19 12:52:39 +0300

    Add project files.

---
+++ MyApp.Domain/Entities/AppUser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyApp.Domain.Entities
+{
+    public class AppUser
+    {
+        public Guid Id { get; set; }
+
+        public string FirstName { get; set; } = default!;
+
+        public string LastName { get; set; } = default!;
+
+        public string Email { get; set; } = default!;
+
+        public string PasswordHash { get; set; } = default!;
+
+        public string Role { get; set; } = "User";
+
+        public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+    }
+}
