ad48ceece3190731da16e046ddde1f0075260078
Mina Karaçay <minakaracayy@gmail.com>
2026-06-12 12:06:50 +0300

    refresh token db entity

---
+++ MyApp.Domain/Entities/RefreshToken.cs
@@ -0,0 +1,16 @@
+﻿using MyApp.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyApp.Domain.Entities
+{
+    public class RefreshToken : BaseEntity
+    {
+        public string Token { get; set; }
+        public Guid UserId { get; set; }
+        public User User { get; set; }
+        public DateTime ExpireAt { get; set; }
+        public bool IsRevoked { get; set; }
+    }
+}
